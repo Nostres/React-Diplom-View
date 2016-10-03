6a61e1a2a6ee06cdafb4b09c77c056b7a7f6ab03
i.borsuk <borsuk.ivan.93@gmail.com>
2016-10-03 19:51:12 +0300

    init configuration for webpack and added babel loader

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -18,6 +18,19 @@ module.exports = {
         filename: "build.js"
     },
 
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+
     // resolve: {
     //     modulesDirectories: [
     //         'src',
