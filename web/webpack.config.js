5a6aff56ddf25e37b0d28ac9a7d112cf9f1dabc2
i.borsuk <borsuk.ivan.93@gmail.com>
2016-10-04 17:12:46 +0300

    add transform pluggin

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -7,7 +7,7 @@ const output = path.join(__dirname, './target/static');
 
 module.exports = {
 
-    // context: path.resolve(__dirname, './'),
+    context: path.resolve(__dirname, './'),
 
     entry: [
         './src/index.js'
@@ -21,30 +21,31 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.js$/,
+                test: /\.js$|\.jsx$/,
                 exclude: /(node_modules)/,
                 loader: 'babel',
                 query: {
-                    presets: ['es2015']
+                    plugins: ['transform-decorators-legacy'],
+                    presets: ['es2015', 'react', "stage-1"]
                 }
             }
         ]
     },
 
-    // resolve: {
-    //     modulesDirectories: [
-    //         'src',
-    //         'node_modules'
-    //     ],
-    //     alias: {
-    //         '/src': './src'
-    //     },
-    //     extensions: ['', '.json', '.js', '.jsx']
-    // },
+    resolve: {
+        modulesDirectories: [
+            'src',
+            'node_modules'
+        ],
+        alias: {
+            '/src': './src'
+        },
+        extensions: ['', '.json', '.js', '.jsx']
+    },
 
-    // resolveLoader: {
-    //     modulesDirectories: ['node_modules']
-    // },
+    resolveLoader: {
+        modulesDirectories: ['node_modules']
+    },
 
     plugins: [
         new HtmlWebpackPlugin({
